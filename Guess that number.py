83448866a3e19513901339657360dafbfac0a60b
HristoTodorov-thecreator <therandombulgarianguy@gmail.com>
2024-09-28 11:13:18 +0300

    Update Guess that number.py

--- Guess that number.py
+++ Guess that number.py
@@ -2,16 +2,24 @@ import random
 
 computer_random = random.randint(1,100)
 
+history = []
 while True:
     player_input = input('Guess the Number (1-100): ')
     if not player_input.isdigit():
         print('Invalid')
         continue
     g = int(player_input)
+    history.append(g)
     if g == computer_random:
         print('You guess it')
         break
+
+
     elif g > computer_random:
         print('Too high')
     else:
         print('Too low')
+
+    print(f'Your guesses so far: {history}')
+
+
